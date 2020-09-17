8d16f2a8679599e2fb657e79c41248481509e0aa
alexandraskach <skach.alexandra@gmail.com>
2020-09-17 11:14:24 +0300

    api

---
+++ api/db/mongoose.js
@@ -0,0 +1,20 @@
+//This file will handle connection logic to the mongoDB database
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TaskManager', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("Connected to MongoDB succesfully");
+
+}).catch((e) => {
+    console.log("Error while attempting to connect to MongoDB");
+    console.log(e);
+});
+//to prevent deprectation warnings (from MongoDB native driver)
+
+mongoose.set('useCreateIndex',true);
+mongoose.set('useFindAndModify',false);
+
+module.exports = {
+    mongoose
+};
